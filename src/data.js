6c3b448c9508bf7842da5426356d0c912ab3d766
geraxgera <gera.konradi@gmail.com>
2023-02-08 13:40:48 +0100

    comment

---
+++ src/data.js
@@ -0,0 +1,17 @@
+import avatar from './images/avatar-the-way-of-the-water.webp'
+import bg from './images/peakpx.jpg'
+
+export const DATA = [
+	{
+		name: 'Avatar',
+		logo: avatar,
+		mainImage: bg,
+		year: 2022,
+		limitAge: '16+',
+		rating: '8.89',
+		duration: '1h 56min',
+		description:
+			'Mehr als zehn Jahre nachdem der ehemalige Soldat Jake Sully mithilfe des „neuronalen Netzwerkes“ von Pandora, genannt Eywa, seinen menschlichen Körper verlassen hat, endgültig mit seinem Avatar vereint wurde und seither selbst ein Na’vi ist, haben er und Neytiri eine Familie gegründet. Ihre Kinder sind ihr Ältester Neteyam, sein Bruder Lo’ak und deren kleine Schwester Tuktirey, genannt Tuk. Sie haben auch den Menschenjungen Miles „Spider“ Socorro, dessen Vater der verstorbene Miles Quaritch ist, und Na’vi-Teenager Kiri, deren Mutter der Avatar von Grace Augustine ist, in ihre Familie aufgenommen.',
+		
+	},
+]
