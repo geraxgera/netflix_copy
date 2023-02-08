edcd53aca7d69fcc53cb01c5fbbe83f844d896cf
geraxgera <gera.konradi@gmail.com>
2023-02-08 13:48:08 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -7,11 +7,11 @@ export const DATA = [
 		logo: avatar,
 		mainImage: bg,
 		year: 2022,
-		limitAge: '16+',
+		limitAge: '14+',
 		rating: '8.89',
-		duration: '1h 56min',
+		duration: '3h 23min',
 		description:
-			'Mehr als zehn Jahre nachdem der ehemalige Soldat Jake Sully mithilfe des „neuronalen Netzwerkes“ von Pandora, genannt Eywa, seinen menschlichen Körper verlassen hat, endgültig mit seinem Avatar vereint wurde und seither selbst ein Na’vi ist, haben er und Neytiri eine Familie gegründet. Ihre Kinder sind ihr Ältester Neteyam, sein Bruder Lo’ak und deren kleine Schwester Tuktirey, genannt Tuk. Sie haben auch den Menschenjungen Miles „Spider“ Socorro, dessen Vater der verstorbene Miles Quaritch ist, und Na’vi-Teenager Kiri, deren Mutter der Avatar von Grace Augustine ist, in ihre Familie aufgenommen.',
+		'Mehr als zehn Jahre nachdem der ehemalige Soldat Jake Sully mithilfe des „neuronalen Netzwerkes“ von Pandora, genannt Eywa, seinen menschlichen Körper verlassen hat, endgültig mit seinem Avatar vereint wurde und seither selbst ein Na’vi ist, haben er und Neytiri eine Familie gegründet. Ihre Kinder sind ihr Ältester Neteyam, sein Bruder Lo’ak und deren kleine Schwester Tuktirey, genannt Tuk. Sie haben auch den Menschenjungen Miles „Spider“ Socorro, dessen Vater der verstorbene Miles Quaritch ist, und Na’vi-Teenager Kiri, deren Mutter der Avatar von Grace Augustine ist, in ihre Familie aufgenommen.',
 		
 	},
 ]
